b75748978d397a8a4e6eac1965e9a17ffb8fd488
Ryoya Ito <cmt14010@nitech.ac.jp>
2023-11-10 15:55:48 +0900

    mainのbbbを変更

--- app/bbb.java
+++ app/bbb.java
@@ -2,13 +2,14 @@ public class bbb {
     int b =0;
     int B = 10;
     int bB = b+B;
-
-    String str2 = "ブランチを使ってみてます";
-    int bb = 200000;
-
-    int BB = 5000000;
-
-    int bbBB = BB - bb;
-
     int aaaaaaaaaaa;
+    String str2 = "ブランチを使ってみてます";
+
+    //コメントアウト
+    int notB;
+    //notB is B
+
+
+
+
 }
